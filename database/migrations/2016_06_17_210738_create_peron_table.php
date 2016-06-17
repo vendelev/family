41be2bb81c641365bf5756634313b4aa15773c43
vendelev <vend_arty@mail.ru>
2016-06-18 00:10:42 +0300

    создание таблицы person

---
+++ database/migrations/2016_06_17_210738_create_peron_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePeronTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('person', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('person');
+    }
+}
