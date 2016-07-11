be32a5d6aee954c8621c7e48b30f660cb90e3e74
vendelev <vend_arty@mail.ru>
2016-07-12 00:31:21 +0300

    Настроил локальный конфиг

--- config/cache.php
+++ config/cache.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('CACHE_DRIVER', 'file'),
+    'default' => env('CACHE_DRIVER', $_ENV['CACHE_DRIVER']),
 
     /*
     |--------------------------------------------------------------------------
